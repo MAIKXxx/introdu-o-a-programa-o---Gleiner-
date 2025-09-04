4dabb1ac7892dbb5845821c034a0eb76e3eff01c
Maicon Goulart <goulartoliveiramaicon@gmail.com>
2025-09-04 10:21:42 -0300

    Add files via upload

---
+++ a=0.py
@@ -0,0 +1,15 @@
+a=0
+num=0
+contag=0
+while a==20 :
+    numa = int(input("digite o numero: "))
+
+    if numa>=0 : 
+        numa+= num
+    else : 
+        contag+=1
+    
+    a+=1
+
+print("As somas de positivos são , "numa)
+print("São " ,contag, "numeros negativos.")
