8e73c86c35e7f1d5992abc4b63211f0a08b55ab1
Maicon Goulart <goulartoliveiramaicon@gmail.com>
2025-09-16 08:31:10 -0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,51 @@
-# Loop para percorrer os números de 1 a 50
-for num in range(2, 51, 2):
-    print(num)
+def soma(a, b):
+    return a + b
+
+def subtração(a, b):
+    return a -  b
+
+def multiplicação(a, b):
+    return a * b
+
+def divisão(a, b):
+    if b == 0:
+        return "erro: divisão por zero!"
+    return a / b 
+
+def par_ou_impar(n):
+    return "par" if n % 2 == 0 else "impar"
+
+def primo(n):
+    if n <= 1:
+        return "Não é primo"
+    for i in range (2, int(n**0.5) + 1):
+        if n % i == 0:
+            return "Não é primo"
+        return "É primo"
+    
+    def fatorial(n):
+        if n < 0:
+            return "Não existe fatorial de número negativo!"
+        fat = 1 
+        for i in range(1, n + 1):
+            fat *= i
+        return fat 
+    
+while True:
+    print("\n=== MENU DE OPERAÇÕES ===")
+    print("1 - Soma")
+    print("2 - subtração")
+    print("3 - multiplicação")
+    print("4 - divisão")
+    print("5 - par ou impar")
+    print("6 - número primo")
+    print("7 - fatorial")
+    print("0 - sair")
+    
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "0":
+        print("saindo... até mais!")
+        break
+
+    # para operações com dois 
